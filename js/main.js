bd982db4afe15e8ab446e4b96bc433c443b15831
khakholin <khakholin@mail.ru>
2019-10-24 17:01:47 +0700

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(window).load(function(){
-    /* обработка нажатия на кнопку закрытия (работает и без нее, т.к. она не является частью навигации)
-    $('.menu__nav_phone').on('click', function(){
+        $('.menu__nav_phone').on('click', function(){
         $('.navigation').toggleClass('navigation_active');
         $('.menu-link-open').toggleClass('menu-link-open_active');
         $('.menu-link-close').toggleClass('menu-link-close_active');
     });
-    */
+    /*
     $(document).mouseup(function (e){ // событие клика по веб-документу
 		var div = $(".navigation_active"); // тут указываем ID элемента
 		if (!div.is(e.target) && div.has(e.target).length === 0) {
@@ -13,5 +12,5 @@ $(window).load(function(){
             $('.menu-link-open').toggleClass('menu-link-open_active');
             $('.menu-link-close').toggleClass('menu-link-close_active');
 		}
-	});
+	})*/
 });
