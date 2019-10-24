bf46188e04fa56b6acb38938c5d219738db62ce8
khakholin <khakholin@mail.ru>
2019-10-24 12:19:26 +0700

    closing navigation by clicking outside an element

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 $(window).load(function(){
+    /* обработка нажатия на кнопку закрытия (работает и без нее, т.к. она не является частью навигации)
     $('.menu__nav_phone').on('click', function(){
         $('.navigation').toggleClass('navigation_active');
         $('.menu-link-open').toggleClass('menu-link-open_active');
         $('.menu-link-close').toggleClass('menu-link-close_active');
     });
-
+    */
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".navigation_active"); // тут указываем ID элемента
+		if (!div.is(e.target) && div.has(e.target).length === 0) {
+            $('.navigation').toggleClass('navigation_active');
+            $('.menu-link-open').toggleClass('menu-link-open_active');
+            $('.menu-link-close').toggleClass('menu-link-close_active');
+		}
+	});
 });
